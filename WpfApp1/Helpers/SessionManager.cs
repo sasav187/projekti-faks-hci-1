8fc0204e2feed9fccc648e7702fd443c6bc539f9
sasav187 <sasavujancevic@gmail.com>
2025-07-12 17:35:02 +0200

    minor changes

--- WpfApp1/Helpers/SessionManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using ProdavnicaApp.Models;
-
-namespace ProdavnicaApp.Helpers
-{
-    public static class SessionManager
-    {
-        public static Korisnik? CurrentUser { get; set; }
-    }
-}
